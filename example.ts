086532fc8c1b2085fc2fba53ee07c9dbaadaffba
stsysd <000stsysd000@gmail.com>
2022-03-15 03:52:16 +0900

    :up: up to v0.4.7

--- example.ts
+++ example.ts
@@ -1,4 +1,4 @@
-import { select } from "https://raw.githubusercontent.com/stsysd/dore/v0.4.5/mod.ts";
+import { select } from "https://raw.githubusercontent.com/stsysd/dore/v0.4.7/mod.ts";
 
 const selected = await select(["foo", "bar", "baz"]);
 console.log("SELECTED:", selected);
